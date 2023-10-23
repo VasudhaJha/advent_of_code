d835f90e4e2a3688cdb0ba0f66cb6ec6c60f886e
Vasudha Jha <reachvasudha27@gmail.com>
2023-10-23 14:42:51 -0700

    day 4 solution 2015 python

---
+++ 2015/python/day-04/part1/main.py
@@ -0,0 +1,19 @@
+import hashlib
+
+def lowest_number(secret_key: str):
+    current_number = 1
+
+    while True:
+        input_string = secret_key + str(current_number)
+        hash = hashlib.md5(input_string.encode()).hexdigest()
+
+        if hash[:5] == "00000":
+            print(f"lowest number: {current_number}")
+            break
+        current_number += 1
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as input:
+        secret_key = input.read()
+        lowest_number(secret_key)
+    
